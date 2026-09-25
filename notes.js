3e4a24769883a5f575626f5c25d2d1e5ff669497
manjunath k <manjunath.kiudanavar@gmail.com>
2026-09-26 02:05:40 +0530

    Update README

--- notes.js
+++ notes.js
@@ -5,3 +5,28 @@
 // after installing the packages it shows the file name 
 // then we have to give the command cd filename
 // then npm start
+// when we are using create my react app we should use npm start to start the execution 
+// when we use another tag we should wrap it with <div></div>
+// how do we write javascript in the jsx ? function App(){
+//     let a=2;
+//     return(
+       
+//         <div>
+//              to get the value we use curly braces {a} it gives us 2
+//              {a+2} this gives us addition 
+//         <h1>Hello iam component</h1>
+//         <p> this is the sample of react component</p>
+//         <p> this is the sample of react component of manjunath</p>
+//         </div>
+//     )
+//  }
+// how do i use css and inline css?
+//  <div>
+//              {a+2}
+//         <h1 className='main'>Hello iam component</h1>
+//         <p className= 'main'> this is the sample of react component</p>
+//         <p style={{backgroundColor: "blue",color: "white"}}> this is the sample of react component of manjunath</p>
+//         </div>
+//     )
+//  }
+//  export default App;
