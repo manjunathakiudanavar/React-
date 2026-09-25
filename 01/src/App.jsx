9813cb37f71dfcba2dea2777021abe34606c9519
manjunath k <manjunath.kiudanavar@gmail.com>
2026-09-26 01:45:16 +0530

    react css and writing javascript in jsx

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -1,6 +1,16 @@
+ import './app.css'
+
+
  function App(){
+    let a=2;
     return(
-        <h1>Hello iam component</h1>
+       
+        <div>
+             {a+2}
+        <h1 className='main'>Hello iam component</h1>
+        <p className= 'main'> this is the sample of react component</p>
+        <p style={{backgroundColor: "blue",color: "white"}}> this is the sample of react component of manjunath</p>
+        </div>
     )
  }
  export default App;
