9f99d093ba1c6f3464b7fccd7c14f04a4438e4ec
manjunath k <manjunath.kiudanavar@gmail.com>
2026-09-25 17:13:43 +0530

    first react component

---
+++ notes.js
@@ -0,0 +1,7 @@
+//  what react is react? 
+//  react is the javascript library which helps to create the user interface
+//  how to create react app 
+//  in the terminal use the command npx create-react-app "name"
+// after installing the packages it shows the file name 
+// then we have to give the command cd filename
+// then npm start
